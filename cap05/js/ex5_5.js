4390252eb35b452bdca36bcbbec32f85f5b5ea0b
Gabriel Schug <gabrielschug@hotmail.com>
2025-12-23 16:39:00 -0300

    Capitulo 5_8

--- cap05/js/ex5_5.js
+++ cap05/js/ex5_5.js
@@ -9,21 +9,23 @@ let valorTotal = 0
 frm.addEventListener('submit', (e) => {
   e.preventDefault()
 
+  // Entrada de dados
   const descricao = frm.inDescricao.value
   const valor = Number(frm.inValor.value)
 
-  numContas++
-  valorTotal += valor
+  // Processamento
+  numContas++ // Incrementa o número de contas
+  valorTotal += valor // Acumula o valor total
+
 
   resposta = resposta + descricao + " - R$: " + valor.toFixed(2) + " \n"
 
+  // Saída de dados
   resp1.innerText = `${resposta} ---------------------------------`
   resp2.innerText = `${numContas} Conta(s) | Total R$: ${valorTotal.toFixed(2)}`
 
+  // Limpa os campos e posiciona o cursor em inDescricao
   frm.inDescricao.value = ""
   frm.inValor.value = ""
   frm.inDescricao.focus()
-
-
-
 })
