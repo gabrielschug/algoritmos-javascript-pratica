5c8109efa9ae0b1a016d727af8215cc9ab9849da
Gabriel Schug <gabrielschug@hotmail.com>
2026-04-15 23:04:24 -0300

    Capitulo 9_3

--- cap09/js/ex9_2.js
+++ cap09/js/ex9_2.js
@@ -5,14 +5,14 @@ const imgClube = document.querySelector('#imgClube')
 
 const trocarClube = () => {
     const clubes = [
-        'brasil',
-        'pelotas',
-        'farroupilha'
+        'Brasil',
+        'Pelotas',
+        'Farroupilha'
     ]
 
     let selecao
-    for (let i = 0 ; i < inRadios.length; i++) {
-        if( inRadios[i].checked) {
+    for (let i = 0; i < inRadios.length; i++) {
+        if (inRadios[i].checked) {
             selecao = i
             break
         }
@@ -21,10 +21,10 @@ const trocarClube = () => {
     if (selecao < clubes.length) {
         divTitulo.className = `row cores-${clubes[selecao]}`
         imgClube.src = `img/${clubes[selecao].toLowerCase()}.png`
-        imgClube.className = `img-fluid`
+        imgClube.className = `img-fluid h-75`
         imgClube.alt = `Simbolo do ${clubes[selecao]}`
         localStorage.setItem("clube", clubes[selecao])
-    }   else {
+    } else {
         divTitulo.className = 'row'
         imgClube.className = 'd-none'
         imgClube.alt = ''
@@ -34,9 +34,21 @@ const trocarClube = () => {
 }
 
 for (const inRadio of inRadios) {
-    inRadio.addEventListener("change",trocarClube)
+    inRadio.addEventListener("change", trocarClube)
 }
 
 const verificarClube = () => {
-    
+    const clube = localStorage.getItem("clube")
+    if (!clube) {
+        return
+    }
+    if (clube == 'Brasil') {
+        inRadios[0].checked = true
+    } else if (clube == 'Pelotas') {
+        inRadios[1].checked = true
+    } else if (clube == 'Farroupilha') {
+        inRadios[2].checked = true
+    }
+    trocarClube()
 }
+window.addEventListener("load", verificarClube)
